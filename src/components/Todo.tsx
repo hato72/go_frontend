a78e34a4da9604e8ecce556635fb61ab09876ed0
hato72 <hatotori72@gmail.com>
2024-03-09 16:01:31 +0900

    react app

---
+++ src/components/Todo.tsx
@@ -0,0 +1,25 @@
+import { FormEvent } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+import {
+  ArrowRightOnRectangleIcon,
+  ShieldCheckIcon,
+} from '@heroicons/react/24/solid'
+import useStore from '../store'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+
+
+export const Todo = () => {
+  const {logoutMutation} = useMutateAuth()
+  const logout = async () => {
+    await logoutMutation.mutateAsync()
+  }
+  return (
+    <div>
+      <ArrowRightOnRectangleIcon
+        onClick={logout}
+        className="h-6 w-6 my-6 text-blue-500 cursor-pointer"
+      />
+    </div>
+  )
+}
+
